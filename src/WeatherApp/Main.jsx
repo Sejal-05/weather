80c9cc2a29ac4f20809bdb69da11f26f27302add
Karuna2505 <karunamathur14@gmail.com>
2023-05-25 17:05:47 +0530

    ui-update

--- src/WeatherApp/Main.jsx
+++ src/WeatherApp/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import MainWeatherInfo from "./MainWeatherInfo";
 import ExtraWeatherInfo from "./ExtraWeatherInfo";
 
-function Main(props){
-  const {weatherData} = props;
-    return <main style={{display: "flex"}}>
-    <MainWeatherInfo weatherData={weatherData} />
-    <ExtraWeatherInfo weatherData={weatherData}/>
-</main>
+function Main(props) {
+  const { weatherData } = props;
+  return (
+    <main style={{ display: "flex" }}>
+      <MainWeatherInfo weatherData={weatherData} />
+      <ExtraWeatherInfo weatherData={weatherData} />
+    </main>
+  );
 }
 export default Main;
